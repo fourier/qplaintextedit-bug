1cd58f6d237c3547ede89fd4ff016bc2e4c1b61e
Alexey Veretennikov <fourier@protonmail.ch>
2021-05-09 20:25:44 +0200

    Reduced to minimal example

--- qbug.cpp
+++ qbug.cpp
@@ -10,16 +10,6 @@ class MainWindow : public QPlainTextEdit
 public:
     MainWindow(QWidget *parent) : QPlainTextEdit(parent)
     {
-        this->setFocusPolicy(Qt::NoFocus);
-        this->setReadOnly(true);
-        this->setUndoRedoEnabled(false);
-        this->setMouseTracking(true);
-
-        QPalette p = viewport()->palette();
-        p.setColor(QPalette::Text, QColor("#a9b7c6"));
-        p.setColor(QPalette::Base, QColor("#2b2b2b"));
-        setPalette(p);
-        this->document()->setMaximumBlockCount(2000);
         timer = new QTimer(this);
         connect(timer, &QTimer::timeout, this, QOverload<>::of(&MainWindow::update));
         timer->start(2000);
@@ -28,37 +18,6 @@ public:
     ~MainWindow() { timer->stop(); }
 
     void update() { appendPlainText("some more text from timer\n"); }
-
-    void mousePressEvent(QMouseEvent *e)
-    {
-        clickedAnchor = (e->button() == Qt::LeftButton) ? anchorAt(e->pos()) : QString();
-        QPlainTextEdit::mousePressEvent(e);
-    }
-
-    void mouseReleaseEvent(QMouseEvent *e)
-    {
-        if (e->button() == Qt::LeftButton && !clickedAnchor.isEmpty()) {
-            if (anchorAt(e->pos()) == clickedAnchor) {
-                QDesktopServices::openUrl(QUrl(clickedAnchor, QUrl::TolerantMode));
-                qDebug() << "CLICKED: " << clickedAnchor;
-            }
-            clickedAnchor.clear();
-        }
-        QPlainTextEdit::mouseReleaseEvent(e);
-    }
-
-    void mouseMoveEvent(QMouseEvent *e)
-    {
-        qDebug() << "mouseMoveEvent: " << e->pos();
-        if (!anchorAt(e->pos()).isEmpty()) {
-            qDebug() << " anchor: " << anchorAt(e->pos()) << " clickedAnchor: " << clickedAnchor;
-            viewport()->setCursor(Qt::PointingHandCursor);
-        } else {
-            qDebug() << " no anchor";
-            viewport()->unsetCursor();
-        }
-        QPlainTextEdit::mouseMoveEvent(e);
-    }
 
 private:
     QTimer *timer;
